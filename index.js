dba393c7d3d22a4448140d7287023e8845853dc6
Farid Kurbanov <git@faridk.com>
2019-03-05 23:47:32 -0800

    Added GraphQL server

---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+var typeDefs = [`
+	schema {
+		query: Query
+	}
+
+	type Query {
+		appInfo: AppInfo
+	}
+
+	type AppInfo {
+		appName: String,
+		appVersion: String
+	}
+`];
+
+var resolvers = {
+	Query: {
+		appInfo(root) {
+			return 'TBD';
+		}
+
+	// 	appName(root) {
+	// 	// Security issue, consider hardcoding this value
+	// 	// return process.env.npm_package_name;
+	// 	return 'cvtools.net';
+	// },
+
+	// appVersion(root) {
+	// 	// Security issue
+	// 	return process.env.npm_package_version;
+	// }
+}
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen(4000, () => console.log('Server listening on localhost:4000/graphiql'));
