72e911cb0743f3f47af593f48b7e07759fafdbde
Farid Kurbanov <git@faridk.com>
2019-03-23 23:45:18 -0700

    Receive login credentials through GraphQL Mutation

--- apollo.js
+++ apollo.js
@@ -2,10 +2,15 @@ const { ApolloServer, gql } = require('apollo-server');
 
 var typeDefs = [`
 	schema {
-		query: Query
+		query: Query,
+		mutation: Mutation
 	}
 	type Query {
 		appInfo: AppInfo
+	}
+	type Mutation {
+		login(email: String, pass: String): String,
+		signup(email: String, pass: String): String
 	}
 	type AppInfo {
 		name: String,
@@ -21,6 +26,16 @@ var resolvers = {
 				name: process.env.npm_package_name,
 				version: process.env.npm_package_version
 			}
+		}
+	},
+	Mutation: {
+		login: async (_, { email, pass }) => {
+			console.log(`\nLogin\ne-mail: ${email}\npass: ${pass}\n`);
+			return 'Login successful';
+		},
+		signup: async (_, { email, pass }) => {
+			console.log(`\nSign Up\ne-mail: ${email}\npass: ${pass}\n`);
+			return 'Signup successful';
 		}
 	}
 };
